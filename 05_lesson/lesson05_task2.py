a0064f03669d9d08e39a91759c83a0d8b851d9e7
Ваше Имя <kochetova6666@gmail.com>
2025-09-13 16:50:12 +0300

    Выполнены задания урока 5

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+
+driver.get("http://uitestingplayground.com/dynamicid")
+driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary").click()
