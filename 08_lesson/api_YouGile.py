85836740d875761c48d7a6222d1fbc2b6c0e54ad
Ваше Имя <kochetova6666@gmail.com>
2025-10-09 18:48:22 +0300

    Выполнены задания урока 8

---
+++ 08_lesson/api_YouGile.py
@@ -0,0 +1,70 @@
+import requests
+
+
+class YouGile:
+    def __init__(self, url, login=None, password=None, companyId=None):
+        self.url = url
+        self.token = self.get_token(login, password, companyId)
+
+    def get_token(self, login, password, companyId):
+        # Получение токена
+        payload = {
+            "login": login,
+            "password": password,
+            "companyId": companyId
+        }
+        resp = requests.post(self.url + 'auth/keys/get', json=payload)
+        response_data = resp.json()
+        return response_data[0]['key']
+
+    def get_projects(self):
+        # Получение списка проектов
+        key = self.token
+        headers = {
+            'Authorization': f'Bearer {key}'
+        }
+        resp = requests.get(self.url + 'projects', headers=headers)
+        return resp.json()['content']
+
+    def create_project(self, title, users):
+        # Создание проекта
+        key = self.token
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+        project = {
+            "title": title,
+            "users": users
+        }
+
+        resp = requests.post(self.url + 'projects', json=project,
+                             headers=headers)
+        return resp
+
+    def get_project_by_id(self, project_id):
+        # Получить проект по ID
+        key = self.token
+        headers = {
+            'Authorization': f'Bearer {key}'
+        }
+        resp = requests.get(self.url + f'projects/{project_id}',
+                            headers=headers)
+        return resp
+
+    def edit_project(self, project_id, new_deleted, new_title, new_users):
+        # Удаление проекта
+        key = self.token
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+        payload = {
+            "deleted": new_deleted,
+            "title": new_title,
+            "users": new_users
+        }
+
+        resp = requests.put(self.url + f'projects/{project_id}',
+                            json=payload, headers=headers)
+        return resp
---
+++ 08_lesson/credentials.py
@@ -0,0 +1,31 @@
+url = 'https://ru.yougile.com/api-v2/'
+
+user_role = "admin"
+
+user_uuid = "6ba44dad-7fea-44a1-ba09-9b292d652005"
+
+wrong_uuid = "14852fgfdf1455-dffd"
+
+name = "Api"
+
+login = "quepuhagruvu-1932@yopmail.com"
+
+password = "Yh48-3_G6f"
+
+companyId = "e9ff0f90-85ed-4790-b619-b0830df98575"
+
+new_project = "SkyPro"
+
+empty_title = ""
+
+testing_title = "SkyPro_test"
+
+edit_title = "SkyPro_edit"
+
+edited_title = "SkyPro_edited"
+
+deleted_status = False
+
+user = {user_uuid: user_role}
+
+wrong_user = {wrong_uuid: user_role}
